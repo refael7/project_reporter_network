00608f2ffee349c748f62c13b11af607680849c0
refael_saidov <refaelsaidov1@gmail.com>
2026-02-25 10:13:23 +0200

    finish OCR_Engine

---
+++ Ingestion_Service/OCR_Engine.py
@@ -0,0 +1,16 @@
+import easyocr
+import logging
+
+class OcrEngine:
+    def __init__(self,logger:logging.Logger):
+        self.logger=logger
+    def  extract_text(self,image_path):
+        try:
+
+            reader = easyocr.Reader(['en'], gpu=False)
+            result = reader.readtext(image_path, detail=0)
+            return " ".join(result)
+
+        except Exception as e:
+            self.logger.error(f"ocr failed for {image_path}: {e}")
+            raise
